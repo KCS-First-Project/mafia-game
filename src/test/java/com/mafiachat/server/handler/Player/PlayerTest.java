e5d2003bfcae2e25c9d185b837f97be1f2dd0e91
JHyun0302 <wogustkfkd11@gmail.com>
2024-01-03 17:36:52 +0900

    :bulb: [Test] vote 테스트 패스
    이유: 단순 id값을 GameManager에서 넘기는 메서드이므로 GameManagerTest에서 작성할 것임

---
+++ src/test/java/com/mafiachat/server/handler/Player/PlayerTest.java
@@ -0,0 +1,25 @@
+package com.mafiachat.server.handler.Player;
+
+import org.mockito.Mock;
+
+class PlayerTest {
+    @Mock
+    Player player;
+
+//    @DisplayName("Client 투표 테스트")
+//    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+//    @ValueSource(strings = {"1", "2", "3"})
+//    public void vote(String playerId) throws IOException {
+//        //given
+//        TestClient testClient = new TestClient(Constant.SERVER_HOST, Constant.SERVER_PORT);
+//        ChatRequest request = ChatRequest.createRequest(Command.VOTE, "");
+//        Player player = new Player();
+//
+//        //when
+//        player.vote(request);
+//
+//        //then
+//        Assertions.assertThat(player.get).isEqualTo(playerId);
+//    }
+
+}
