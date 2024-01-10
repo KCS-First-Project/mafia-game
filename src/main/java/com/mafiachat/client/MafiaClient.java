06eb3095ee9ae5f90b0bb79734ca853755ff6dd6
amanecopse <amanecopse@gmail.com>
2024-01-10 09:47:32 +0900

    feat: 호스트, 포트 설정 다이얼로그 구현
    - setHostAndPortUsingDialog(): 호스트와 포트를 입력하는 다이얼로그를 표시하고 받은 입력값을 반영한다.
    - play 버튼 누를 시에 서버 연결 전에 setHostAndPortUsingDialog()를 호출하여 호스트, 포트 설정하도록 변경.

--- src/main/java/com/mafiachat/client/MafiaClient.java
+++ src/main/java/com/mafiachat/client/MafiaClient.java
@@ -16,127 +16,130 @@ import javax.swing.*;
 
 public class MafiaClient implements ChatConnector {
 
-	public static void main(String[] args) {
-		new MafiaClient();
-	}
-	private String host;
-	private int port=1223;
-	private Socket socket;
-	private JFrame startWindow;
-	private JFrame gameWindow;
-
-	private ArrayList<ChatSocketListener> sListeners = new ArrayList<ChatSocketListener>();
-	MafiaClient() {
-		
-		startWindow = new JFrame("MafiaStart");
-		ImageIcon mafiaIcon = ImageProvider.getInstance().getMafiaIcon();
-		startWindow.setIconImage(mafiaIcon.getImage());
-		startWindow.setSize(500, 400);
-		StartPanel startPanel = new StartPanel();
-		startWindow.add(startPanel);
-		startWindow.setVisible(true);
-		startWindow.setResizable(false);
-		startWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		gameWindow = new JFrame("MafiaChat");
-		gameWindow.setIconImage(mafiaIcon.getImage());
-
-		
-		
-		JPanel contentPane = new JPanel(new BorderLayout());
-		contentPane.setBorder(BorderFactory.createEmptyBorder(3,3,3,3));
-		ChatPanel chatPanel = new ChatPanel(this);
-
-
-		ChatMessageReceiver chatReceiver = new ChatMessageReceiver(this);
-		chatReceiver.setMessageReceiver(chatPanel);
-
-		contentPane.add(chatPanel);
-//		
-		gameWindow.setContentPane(contentPane);
-
-		this.addChatSocketListener(chatPanel);
-		this.addChatSocketListener(chatReceiver);
-
-		
-		gameWindow.setSize(800, 600);
-		gameWindow.setVisible(false);
-		gameWindow.setResizable(false);
-		gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		GamePanel gamePanel = new GamePanel();
-		startPanel.play.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				startWindow.setVisible(false);
-				startWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-				gameWindow.setVisible(true);
-				connect();// 커넥트 위치 수정
-				
-			}	
-		});
-	
-		
-	}
-
-
-	public boolean connect() {
-		if(socketAvailable()) return true;
-		try 
-		{
-			socket = new Socket(host, port);
-			for(ChatSocketListener lsnr: sListeners) {
-				lsnr.socketConnected(socket);
-				
-			}
-			return true;
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
+    public static void main(String[] args) {
+        new MafiaClient();
     }
-	public void disConnect() 
-	{
-		if(!(socket.isClosed())) 
-		{
-			try 
-			{
-				socket.close();
-			} catch(IOException ex) 
-			{
-			}
-		}
-	}
 
-	@Override
-	public Socket getSocket() {
-		return null;
-	}
+    private String host;
+    private int port = 1223;
+    private Socket socket;
+    private JFrame startWindow;
+    private JFrame gameWindow;
+
+    private ArrayList<ChatSocketListener> sListeners = new ArrayList<ChatSocketListener>();
+
+    MafiaClient() {
+
+        startWindow = new JFrame("MafiaStart");
+        ImageIcon mafiaIcon = ImageProvider.getInstance().getMafiaIcon();
+        startWindow.setIconImage(mafiaIcon.getImage());
+        startWindow.setSize(500, 400);
+        StartPanel startPanel = new StartPanel();
+        startWindow.add(startPanel);
+        startWindow.setVisible(true);
+        startWindow.setResizable(false);
+        startWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow = new JFrame("MafiaChat");
+        gameWindow.setIconImage(mafiaIcon.getImage());
 
 
-	public boolean socketAvailable() 
-	{
-		return !(socket == null || socket.isClosed());
-	}
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+        ChatPanel chatPanel = new ChatPanel(this);
 
-	@Override
-	public void invalidateSocket() {
 
-	}
+        ChatMessageReceiver chatReceiver = new ChatMessageReceiver(this);
+        chatReceiver.setMessageReceiver(chatPanel);
 
-	@Override
-	public String getName() {
-		return null;
-	}
+        contentPane.add(chatPanel);
+//		
+        gameWindow.setContentPane(contentPane);
 
-	@Override
-	public String getId() {
-		return null;
-	}
-	public void addChatSocketListener(ChatSocketListener lsnr) {
-		sListeners.add(lsnr);
-	}
-	public void removeChatSocketListener(ChatSocketListener lsnr) {
-		sListeners.remove(lsnr);
-	}
+        this.addChatSocketListener(chatPanel);
+        this.addChatSocketListener(chatReceiver);
+
+
+        gameWindow.setSize(800, 600);
+        gameWindow.setVisible(false);
+        gameWindow.setResizable(false);
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        GamePanel gamePanel = new GamePanel();
+        startPanel.play.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setHostAndPortUsingDialog();
+                if (!connect()) {
+                    return;
+                }
+                startWindow.setVisible(false);
+                gameWindow.setVisible(true);
+            }
+        });
+
+
+    }
+
+
+    public boolean connect() {
+        if (socketAvailable()) return true;
+        try {
+            socket = new Socket(host, port);
+            for (ChatSocketListener lsnr : sListeners) {
+                lsnr.socketConnected(socket);
+
+            }
+            return true;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void disConnect() {
+        if (!(socket.isClosed())) {
+            try {
+                socket.close();
+            } catch (IOException ex) {
+            }
+        }
+    }
+
+    @Override
+    public Socket getSocket() {
+        return null;
+    }
+
+
+    public boolean socketAvailable() {
+        return !(socket == null || socket.isClosed());
+    }
+
+    @Override
+    public void invalidateSocket() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    public void addChatSocketListener(ChatSocketListener lsnr) {
+        sListeners.add(lsnr);
+    }
+
+    public void removeChatSocketListener(ChatSocketListener lsnr) {
+        sListeners.remove(lsnr);
+    }
+
+    private void setHostAndPortUsingDialog() {
+        host = JOptionPane.showInputDialog("접속 호스트를 입력하세요.");
+        port = Integer.parseInt(JOptionPane.showInputDialog("접속 포트를 입력하세요."));
+    }
 }
